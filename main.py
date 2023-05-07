1f44964d8c97b821a5e5bd968878063b2c0ee404
p32 <ostik19791108@gmail.com>
2023-05-07 14:48:28 +0300

    sdfs7

--- main.py
+++ main.py
@@ -1,1 +1,74 @@
-print('hi')
+#my_dict = {
+  #  'Macbook': 'pro',
+ #   'Tesla': 'x'
+#}
+#key = 'Honda'
+#value = 'Cv'
+#my_dict[key] = value
+#print(my_dict)
+#print(my_dict['Macbook'])
+#thisdict = dict(name = 'John', age = 36, country = 'Norway')
+
+#print(thisdict)
+#thisdict = {
+#    'brand': 'ford',
+#    'electric': False,
+#    'year': 1964,
+#    'colors': ['red','white','yellow']
+#}
+#del thisdict
+#thisdict.pop('year')
+#print(thisdict['colors'])
+#print(thisdict.items())
+#print(thisdict)
+#thisdict = dict(name = 'John', age = 36, country = 'Norway')
+#for x in thisdict:
+   # print(thisdict[x])
+
+#for x,y in thisdict.items():
+    #print(x,y)
+
+
+#thisdict = {
+#'brand': 'ford',
+#'electric': False,
+#'year': 1964,
+#'colors': ['red','white','yellow']
+#}
+#my_dict = thisdict
+#print(my_dict)
+#my_dict['new key'] = 'new_value'
+#print(thisdict)
+#print(my_dict)
+#user = my_dict['Anna']['Phone']
+#my_dict['Anna'] --> {
+#    'phone': '2222',
+#    'insta': '@312'
+#}
+#print(user)
+
+
+my_dict = {'Nick':
+           {'phone':'4234234233',
+           'insta' : '@312'
+            },
+           'Anna':{'phone' : '123123123',
+                   'insta' : '@3213'
+                   },
+           'Rick':{'phone': '2313123',
+                   'insta': '@3213'
+                  }
+                }
+# someone = input("Введіть ім'я")
+ #if someone == my_dict['Nick'] or someone == my_dict['Anna']:
+  #   print(someone{'phone'}, someone{'insta'})
+ #else:
+
+  #   print(someone{'phone'}, someone{'insta'})
+
+
+
+#user = my_dict[input()][input()]
+#print(user)
+
+user =
